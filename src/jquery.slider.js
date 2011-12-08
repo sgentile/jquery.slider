9a36b5cc5903078f2336e7a26b32b2e29bad0692
Steve Gentile <steven.gentile@gmail.com>
2011-12-07 19:15:37 -0500

    mock up sample

---
+++ src/jquery.slider.js
@@ -0,0 +1,6 @@
+/*
+* slider 1.0.0 - jQuery UI Widget
+* https://github.com/sgentile/jquery.slider
+* Copyright (c) 2011 Steve Gentile (http://twitter.com/stevemgentile)
+* Dual licensed under the MIT and GPL licenses
+*/
